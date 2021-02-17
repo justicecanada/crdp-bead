dae21e2b90ce58675efec7d9d1db1cd796d61d4b
Thomas Gohard <thomasgohard@users.noreply.github.com>
2021-02-17 14:44:37 -0500

    Update JavaScript to generate QR code

--- js/generate-qrcode.js
+++ js/generate-qrcode.js
@@ -1,74 +1,141 @@
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-    width: 150,
-    height: 150,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.L
-});
+function generateQRCode() {
+    const formId = "CRDPRegistration";
+    const qrCodeId = "qrcode";
+    const fieldNames = {
+        "Cno": {
+            fieldType: "Number",
+            defaultValue: "0",
+            valueSource: "CourtNoTo",
+            sourceType: "id"
+        },
+        "RNo": {
+            fieldType: "Number",
+            defaultValue: "0",
+            valueSource: "RegistryNoTo",
+            sourceType: "id"
+        },
+        "TCNo": {
+            fieldType: "Number",
+            defaultValue: "0",
+            valueSource: "CourtNoFrom",
+            sourceType: "id"
+        },
+        "TRNo": {
+            fieldType: "Number",
+            defaultValue: "0",
+            valueSource: "RegistryNoFrom",
+            sourceType: "id"
+        },
+        "Fee": {                            // Q (FeeQuarterly==true) || E (FeeExempt==true || FeeTransferred==true)
+            fieldType: "Option",
+            valueSource: "FeeType",
+            sourceType: "name"
+        },
+        "Prv": {
+            fieldType: "String",
+            valueSource: "Province",
+            sourceType: "id"
+        },
+        "DFl": {
+            fieldType: "Date",
+            fieldFormat: "YYYY-MM-DD",
+            valueSource: "DateFiled",
+            sourceType: "id"
+        },
+        "DMr": {
+            fieldType: "Date",
+            fieldFormat: "YYYY-MM-DD",
+            valueSource: "DateMarriage",
+            sourceType: "id"
+        },
+        "Jnt": {                                    // true (JointApplicant1 && JointApplicant2) || false
+            fieldType: "Boolean",
+            valueSource: "Applicant1Type",
+            sourceType: "name"
+        },
+        "ASx": {                                    // M (male) || F (female) || A (another gender) || I (Information not available)
+            fieldType: "Option",
+            valueSource: "Applicant1Gender",
+            sourceType: "name"
+        },
+        "ASne": {
+            fieldType: "String",
+            valueSource: "Applicant1Surname",
+            sourceType: "id"
+        },
+        "AGne": {
+            fieldType: "String",
+            valueSource: "Applicant1GivenNames",
+            sourceType: "id"
+        },
+        "ADob": {
+            fieldType: "Date",
+            fieldFormat: "YYYY-MM-DD",
+            valueSource: "Applicant1DateBirth",
+            sourceType: "id"
+        },
+        "RSx": {                                    // M (male) || F (female) || A (another gender) || I (Information not available)
+            fieldType: "Option",
+            valueSource: "Applicant2Gender",
+            sourceType: "name"
+        },
+        "RSne": {
+            fieldType: "String",
+            valueSource: "Applicant2Surname",
+            sourceType: "id"
+        },
+        "RGne": {
+            fieldType: "String",
+            valueSource: "Applicant2GivenNames",
+            sourceType: "id"
+        },
+        "RDob": {
+            fieldType: "Date",
+            fieldFormat: "YYYY-MM-DD",
+            valueSource: "Applicant2DateBirth",
+            sourceType: "id"
+        },
+        "Dsi": {
+            fieldType: "Date",
+            fieldFormat: "YYYY-MM-DD",
+            valueSource: "DateSigned",
+            sourceType: "id"
+        }
+    };
+    var qrcodeData = "";
 
-function makeCode() {
-    var elText = [];
+    Object.keys(fieldNames).forEach(function(key) {
+        var fieldSpecs = fieldNames[key];
+        var fieldValue = null;
 
-    elText.push("CNo:");
-    elText.push(($("#txtCourtNo").val()) ? $("#txtCourtNo").val() : "0");
-    elText.push(";");
-    elText.push("RNo:");
-    elText.push(($("#txtRegNo").val()) ? $("#txtRegNo").val() : "0");
-    elText.push(";");
-    elText.push(($("#txtTransferCourtNo").val()) ? "TCNo:" : "");
-    elText.push(($("#txtTransferCourtNo").val()) ? $("#txtTransferCourtNo").val() : "");
-    elText.push(($("#txtTransferCourtNo").val()) ? ";" : "");
-    elText.push(($("#txtTransferRegNo").val()) ? "TRNo:" : "");
-    elText.push(($("#txtTransferRegNo").val()) ? $("#txtTransferRegNo").val() : "");
-    elText.push(($("#txtTransferRegNo").val()) ? ";" : "");
-    elText.push("Fee:");
-    elText.push(($("#rbExempt").prop("checked") || $("#rbExemptAppTrans").prop("checked")) ? "E" : "Q");
-    elText.push(";");
-    elText.push("DFl:");
-    elText.push(($("#txtDateFiled").val()) ? $("#txtDateFiled").val() : " ");
-    elText.push(";");
-    elText.push("DMr:");
-    elText.push(($("#txtDateofMarriage").val()) ? $("#txtDateofMarriage").val() : " ");
-    elText.push(";");
-    elText.push("ASx:");
-    elText.push(($("#rbApplicantMale").prop("checked")) ? "M" : "");
-    elText.push(($("#rbApplicantFemale").prop("checked")) ? "F" : "");
-    elText.push(";");
-    elText.push("Jnt:");
-    elText.push(($("#rbApplicantJoint").prop("checked")) ? "true" : "false");
-    elText.push(";");
-    elText.push("ASne:");
-    elText.push(($("#txtApplicantSurname").val()) ? $("#txtApplicantSurname").val() : " ");
-    elText.push(";");
-    elText.push("AGne:");
-    elText.push(($("#txtApplicantGivenName").val()) ? $("#txtApplicantGivenName").val() : " ");
-    elText.push(";");
-    elText.push(($("#txtApplicantMiddleName").val()) ? "AMne:" : "");
-    elText.push(($("#txtApplicantMiddleName").val()) ? $("#txtApplicantMiddleName").val() : "");
-    elText.push(($("#txtApplicantMiddleName").val()) ? ";" : "");
-    elText.push(($("#txtApplicantDateofBirth").val()) ? "ADob:" : "");
-    elText.push(($("#txtApplicantDateofBirth").val()) ? $("#txtApplicantDateofBirth").val() : "");
-    elText.push(($("#txtApplicantDateofBirth").val()) ? ";" : "");
-    elText.push("RSx:");
-    elText.push(($("#rbRespondentMale").prop("checked")) ? "M" : "");
-    elText.push(($("#rbRespondentFemale").prop("checked")) ? "F" : "");
-    elText.push(";");
-    elText.push("RSne:");
-    elText.push(($("#txtRespondentSurname").val()) ? $("#txtRespondentSurname").val() : " ");
-    elText.push(";");
-    elText.push("RGne:");
-    elText.push(($("#txtRespondentGivenName").val()) ? $("#txtRespondentGivenName").val() : " ");
-    elText.push(";");
-    elText.push(($("#txtRespondentMiddleName").val()) ? "RMne:" : "");
-    elText.push(($("#txtRespondentMiddleName").val()) ? $("#txtRespondentMiddleName").val() : "");
-    elText.push(($("#txtRespondentMiddleName").val()) ? ";" : "");
-    elText.push(($("#txtRespondentDateofBirth").val()) ? "RDob:" : "");
-    elText.push(($("#txtRespondentDateofBirth").val()) ? $("#txtRespondentDateofBirth").val() : "");
-    elText.push(($("#txtRespondentDateofBirth").val()) ? ";" : "");
-    elText.push("Dsi:");
-    elText.push(($("#txtDateSigned").val()) ? $("#txtDateSigned").val() : " ");
+        switch(fieldSpecs.sourceType) {
+            case "id":
+                fieldValue = document.getElementById(fieldSpecs.valueSource).value;
+                break;
+            case "name":
+                fieldValue = document.forms[formId].elements.namedItem(fieldSpecs.valueSource).value;
+                break;
+            default:
+                console.log("Error: Unsupported field source.\n\n" + key + ": " + fieldSpecs.sourceType);
+        }
+        
+        qrcodeData += key + ":" + fieldValue + ";";
+    });
 
-    qrcode.makeCode(elText.join(""));
+    // make sure any existing QR code is cleared
+    var qrcode = document.getElementById(qrCodeId);
+    qrcode.removeAttribute("title");
+    while(qrcode.firstChild) {
+        qrcode.removeChild(qrcode.lastChild);
+    }
+    
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        width: 150,
+        height: 150,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.L
+    });
 
-    setAllCookies();
+    qrcode.makeCode(qrcodeData);
 }
